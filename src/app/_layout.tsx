055ef90f46dbb4fb4ed8bd7ebc7cc7a200de5c91
samuelsimoes31 <samuelsimoes31@gmail.com>
2025-03-02 16:48:35 -0300

    Add TT Chocolates font and load custom fonts in app layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,8 +3,18 @@ import { usePushNotifications } from '@/hooks/useNotification';
 import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/native'
 import { theme } from '@/themes'
+import { useFonts } from 'expo-font'
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'TTChocolates-Regular': require('../../assets/fonts/TT Chocolates Trial Regular.otf'),
+    'TTChocolates-Bold': require('../../assets/fonts/TT Chocolates Trial Bold.otf'),
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   // const { expoPushToken} = usePushNotifications();
 
   // useEffect(() => {
