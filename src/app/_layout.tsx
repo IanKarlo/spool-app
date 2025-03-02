8691f73872766128b0ea6b877d6d10833e65a42e
IanKarlo <iankarlots@gmail.com>
2025-03-02 15:18:16 -0300

    some adjusts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from 'expo-router';
+import { usePushNotifications } from '@/hooks/useNotification';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
+  // const { expoPushToken} = usePushNotifications();
+
+  // useEffect(() => {
+  //   if (expoPushToken) {
+  //     console.log('Push token received: ', expoPushToken);
+  //   }
+  // }, [expoPushToken])
+
   return (
     <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
