2818c7cd75fa98bceb230f1623a3e4c4d0a2b870
samuelsimoes31 <samuelsimoes31@gmail.com>
2025-03-09 19:12:22 -0300

    Add go back button functionality to Header component

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,24 +1,45 @@
-import Profile from '@/components/atomics/Profile'
-import { Typography } from '@/components/atomics/Typography'
-import type { Colors } from '@/themes'
-import React from 'react'
-import { View } from 'react-native'
+import { Icon } from "@/components/atomics/Icon";
+import Profile from "@/components/atomics/Profile";
+import { Typography } from "@/components/atomics/Typography";
+import type { Colors } from "@/themes";
+import React from "react";
+import { View, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
 
 interface HeaderProps {
-  name: string
-  color: Colors
-  profileImage: string
-  showGoBackButton?: boolean
+	name: string;
+	color: Colors;
+	profileImage: string;
+	showGoBackButton?: boolean;
 }
 
-export default function Header({ name, profileImage, showGoBackButton = false, color }: HeaderProps) {
-  return (
-    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-      <View>
-        <Typography>Resumo de</Typography>
-        <Typography style={{ fontSize: 28, fontWeight: 'bold' }} color='pink'>{name}</Typography>
-      </View>
-      <Profile uri={profileImage} color={color} />
-    </View>
-  )
+export default function Header({
+	name,
+	profileImage,
+	showGoBackButton = false,
+	color,
+}: HeaderProps) {
+	return (
+		<View
+			style={{
+				flexDirection: "row",
+				justifyContent: "space-between",
+				alignItems: "center",
+			}}
+		>
+			{showGoBackButton ? (
+				<TouchableOpacity onPress={() => router.back()}>
+					<Icon name="arrow-left" size={24} color="black" />
+				</TouchableOpacity>
+			) : (
+				<View>
+					<Typography>Resumo de</Typography>
+					<Typography style={{ fontSize: 28, fontWeight: "bold" }} color={color}>
+						{name}
+					</Typography>
+				</View>
+			)}
+			<Profile uri={profileImage} color={color} />
+		</View>
+	);
 }
