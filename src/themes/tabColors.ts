a2ba862fb623649c3d5a8f5e4d9fa3420a34d525
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-03-15 21:39:29 -0300

    Add new tab colors for educators

--- src/themes/tabColors.ts
+++ src/themes/tabColors.ts
@@ -1,14 +1,16 @@
-import type { Colors } from '@/themes'
-import type { Href } from 'expo-router'
+import type { Colors } from "@/themes";
+import type { Href } from "expo-router";
 
-type HrefString = Href & string
+type HrefString = Href & string;
 
-export const tabColors:  Partial<Record<HrefString, Colors>> = {
-  "/parents/home": 'pink',
-  "/parents/teaching": 'purple',
-  "/parents/therapy": 'green',
-  "/therapist/home": 'purple',
-  "/therapist/alert": 'pink'
-}
+export const tabColors: Partial<Record<HrefString, Colors>> = {
+  "/parents/home": "pink",
+  "/parents/teaching": "purple",
+  "/parents/therapy": "green",
+  "/therapist/home": "purple",
+  "/therapist/alert": "pink",
+  "/educators/home": "purple",
+  "/educators/alerts": "pink",
+};
 
-export type TabColor = keyof typeof tabColors
+export type TabColor = keyof typeof tabColors;
