f225dbb73b35f9370a6a2af9517caa0df07b9f23
JoaoGMSF <jgmsf@cin.ufpe.br>
2025-03-16 12:15:22 -0300

    Add navigation functions and update Home component layout

--- src/app/parents/home/index.tsx
+++ src/app/parents/home/index.tsx
@@ -1,16 +1,27 @@
-import { PageContainer } from '@/components/atomics/PageContainer'
-import { Typography } from '@/components/atomics/Typography'
-import { Link } from 'expo-router'
-import Header from '@/components/molecules/Header'
+import { PageContainer } from "@/components/atomics/PageContainer";
+import { Typography } from "@/components/atomics/Typography";
+import Header from "@/components/molecules/Header";
+import { BigCard } from "@/components/organisms/BigCard";
+import { CarousellList } from "@/components/organisms/CarousellList";
+import { RegisterHistory } from "@/components/organisms/History";
+import { router } from "expo-router";
+
 export default function Home() {
+  function newRegister() {
+    router.push("/parents/home/newRegister");
+  }
+  function viewRegister() {
+    router.push("/parents/home/viewRegister");
+  }
+  function viewHistory() {
+    router.push("/parents/home/viewHistory");
+  }
+
   return (
     <PageContainer>
-      <Header name='John Doe' profileImage='https://github.com/diego3g.png' />
-      <Typography>Home</Typography>
-      <Typography>Home</Typography>
-      <Typography>Home</Typography>
-      <Typography>Home</Typography>
-      <Link href="/parents/home/history">History</Link>
+      <Header name="John Doe" profileImage="https://github.com/diego3g.png" />
+      <BigCard color="darkBlue" fontColor="white" fn={newRegister} />
+      <RegisterHistory cardFn={viewRegister} historyFn={viewHistory} color="blue"/>
     </PageContainer>
-  )
+  );
 }
