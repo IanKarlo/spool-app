22965816a6b036bc2ecc04589df80de65fb37647
JoaoGMSF <jgmsf@cin.ufpe.br>
2025-03-21 00:06:35 -0300

    Add viewTherapist component with profile header and registration history display

---
+++ src/app/parents/therapy/viewTherapist/index.tsx
@@ -0,0 +1,27 @@
+import { PageContainer } from "@/components/atomics/PageContainer";
+import { Typography } from "@/components/atomics/Typography";
+import Header from "@/components/molecules/Header";
+import RegisterCardList from "@/components/organisms/RegisterCardList";
+import { router } from "expo-router";
+
+export default function viewTherapist() {
+  return (
+    <PageContainer>
+      <Header name='John Doe' simpleText="Psicologo" profileImage="https://github.com/diego3g.png" headerType = "profile"/>
+      <Typography
+            style={{
+              fontSize: 20,
+              fontWeight: 600,
+            }}
+            color="darkBlue"
+          >
+            Histórico de Registros
+          </Typography>
+      <RegisterCardList 
+        cardFn={() => router.push("/parents/teaching/viewRegister")}
+        color="lightBlue"
+        fontColor="darkBlue"
+      />
+    </PageContainer>
+  );
+}
