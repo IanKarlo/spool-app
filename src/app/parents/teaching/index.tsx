bcd2609b9b1593fca0e2d60820c0e40a485f0d81
JoaoGMSF <jgmsf@cin.ufpe.br>
2025-03-20 22:20:28 -0300

    Add teaching-related components and update RegisterHistory props for color customization

--- src/app/parents/teaching/index.tsx
+++ src/app/parents/teaching/index.tsx
@@ -1,17 +1,37 @@
-import { PageContainer } from '@/components/atomics/PageContainer'
-import { Typography } from '@/components/atomics/Typography'
-import { Link } from 'expo-router'
-import Header from '@/components/molecules/Header'
-export default function Teaching() {
+import { PageContainer } from "@/components/atomics/PageContainer";
+import { Typography } from "@/components/atomics/Typography";
+import Header from "@/components/molecules/Header";
+import { BigCard } from "@/components/organisms/BigCard";
+import { CarousellList } from "@/components/organisms/CarousellList";
+import { RegisterHistory } from "@/components/organisms/History";
+import { router } from "expo-router";
+
+export default function Home() {
+  function viewTeacher() {
+    router.push("/parents/teaching/viewTeacher");
+  }
+  function teacherList() {
+    router.push("/parents/teaching/teacherList");
+  }
+  function viewCarer() {
+    router.push("/parents/teaching/viewCarer");
+  }
+  function carerList() {
+    router.push("/parents/teaching/carerList");
+  }
+  function viewHistory() {
+    router.push("/parents/home/viewHistory");
+  }
+  function viewRegister() {
+    router.push("/parents/home/viewRegister");
+  }
+
   return (
     <PageContainer>
-      <Header name='John Doe' profileImage='https://github.com/diego3g.png' />
-      <Typography>Teaching</Typography>
-      <Typography>Teaching</Typography>
-      <Typography>Teaching</Typography>
-      <Typography>Teaching</Typography>
-      <Typography>Teaching</Typography>
-      <Link href="/parents/teaching/history">History</Link>
+      <Header name="Mateus" profileImage="https://github.com/diego3g.png" simpleText="Ensino do"/>
+      <CarousellList crFn={teacherList} title='Professores' cardFn={viewTeacher} color="white" fontColor='black' cardColor='lightBlue' cardFontcolor='black'/>
+      <CarousellList crFn={carerList} title='Cuidadores' cardFn={viewCarer} color="white" fontColor='black' cardColor='lightBlue' cardFontcolor='black'/>
+      <RegisterHistory cardFn={viewRegister} historyFn={viewHistory} cardColor="lightBlue" fontCardColor="darkBlue"/>
     </PageContainer>
-  )
+  );
 }
---
+++ src/app/parents/teaching/viewHistory/index.tsx
@@ -0,0 +1,31 @@
+import { PageContainer } from "@/components/atomics/PageContainer";
+import { Typography } from "@/components/atomics/Typography";
+import Header from "@/components/molecules/Header";
+import { BigCard } from "@/components/organisms/BigCard";
+import { CarousellList } from "@/components/organisms/CarousellList";
+import { RegisterHistory } from "@/components/organisms/History";
+import RegisterCardList from "@/components/organisms/RegisterCardList";
+import { router } from "expo-router";
+
+export default function Home() {
+
+
+  return (
+    <PageContainer>
+      <Header name="John Doe" profileImage="https://github.com/diego3g.png" showGoBackButton/>
+      <Typography
+            style={{
+              fontSize: 24,
+              fontWeight: 600,
+              color: "darkBlue",
+            }}
+          >
+            Histórico de Registros
+          </Typography>
+      <RegisterCardList 
+        cardFn={() => router.push("/parents/home/viewRegister")}
+        color="darkBlue"
+      />
+    </PageContainer>
+  );
+}
