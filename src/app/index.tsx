427d8b72d02c91b5f30d023ca1562c434a410265
Samuel Simões <samuelsimoes31@gmail.com>
2025-03-30 13:09:53 -0300

    Refactor Index component to enhance user interface with logo, token input field, and updated button layout for improved navigation and user experience.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,30 +1,47 @@
 import { Button } from '@/components/atomics/Button'
-import { router } from 'expo-router'
+import { useTheme } from 'styled-components'
+import { Link, router } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, View } from 'react-native'
+import { TextField } from '@/components/atomics/TextField'
+const SpoolLogo = require('../../assets/images/spool-blue.png')
 
 export default function Index() {
+  const theme = useTheme()
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 24 }}>
-      <Button
-        text="PARENTS"
-        onPress={() => router.push('./parents')}
-      />
-      <Button
-        text="EDUCATORS"
-        color="purple"
-        onPress={() => router.push('./educators')}
-      />
-      <Button
-        text="THERAPISTS"
-        color="green"
-        onPress={() => router.push('./therapist')}
-      />
-      <Button
-        text="COMPONENTS"
-        color="blue"
-        onPress={() => router.push('./components')}
-      />
+    <View style={{ flex: 1, justifyContent: 'space-between', padding: 24, gap: 24, height: '100%', position: 'relative' }}>
+      <View style={{ gap: 16 }}>
+        <Image source={SpoolLogo} style={{ alignSelf: 'center', marginVertical: 128 }} />
+        <TextField
+          label="Insira seu token"
+          placeholder="Digite seu token aqui"
+        />
+        <Button
+          text="Entrar"
+          color="blue"
+          onPress={() => router.push('./parents')}
+        />
+      </View>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: 16 }}>
+        {__DEV__ && (
+          <Button
+            text="Tokens"
+          color="darkBlue"
+          variant='outlined'
+          style={{ borderWidth: 0 }}
+          onPress={() => router.push('./tokens')}
+        />
+      )}
+      {__DEV__ && (
+        <Button
+          text="Components"
+          color="darkBlue"
+          variant='outlined'
+          style={{ borderWidth: 0 }}
+          onPress={() => router.push('./components')}
+        />
+      )}
+      </View>
     </View>
   )
 }
