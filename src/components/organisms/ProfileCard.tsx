b415ebc3cdf4cb2c1cd1dabcd408b9f5e714d715
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-03-30 22:49:27 -0300

    Adds support for dynamic profile picture in ProfileCard component

--- src/components/organisms/ProfileCard.tsx
+++ src/components/organisms/ProfileCard.tsx
@@ -3,6 +3,8 @@ import { Box } from "../atomics/Box";
 import { Typography } from "../atomics/Typography";
 import { TouchableOpacity, View, type ViewStyle } from "react-native";
 import Profile from "../atomics/Profile";
+import { usePathnameColor } from "@/hooks/usePathnameColor";
+import { useTheme } from "styled-components";
 
 export function ProfileCard({
   color,
@@ -24,17 +26,47 @@ export function ProfileCard({
   info: string;
 }) {
   const width = variation === "normal" ? "100%" : 260;
+  const { tabColor } = usePathnameColor();
+  const theme = useTheme();
+
+  const colorAvatar = theme.colors[tabColor as Colors].split("#")[1];
+
+  const profileImageAvatar = `https://ui-avatars.com/api/?size=64&background=${colorAvatar}&color=fff&name=${encodeURI(
+    name
+  )}`;
 
   return (
     <TouchableOpacity onPress={fn}>
-      <Box style={{ height: 92, padding: 12, width, ...style }} bgColor={'lightBlue'}>
-        <View style={{ display: "flex", flexDirection: "row", gap: 12 }}>
-          <Profile uri="https://github.com/diego3g.png" color={color} singleBorder />
+      <Box
+        style={{ height: 92, padding: 12, width, ...style }}
+        bgColor={"lightBlue"}
+      >
+        <View style={{ display: "flex", flexDirection: "row", gap: 12, alignItems: "center", height: "100%" }}>
+          <Profile
+            uri={profileImageAvatar}
+            color={color}
+            singleBorder
+            size={48}
+          />
           <View style={{ marginVertical: "auto" }}>
-            <Typography color={fontColorName} style={{ fontSize: 18, fontWeight: 600, fontFamily: 'TTChocolates-Medium' }}>
+            <Typography
+              color={fontColorName}
+              style={{
+                fontSize: 18,
+                fontWeight: 600,
+                fontFamily: "TTChocolates-Medium",
+              }}
+            >
               {name}
             </Typography>
-            <Typography color={fontColorInfo} style={{ fontSize: 12, fontWeight: 400, fontFamily: 'TTChocolates-Regular' }}>
+            <Typography
+              color={fontColorInfo}
+              style={{
+                fontSize: 12,
+                fontWeight: 400,
+                fontFamily: "TTChocolates-Regular",
+              }}
+            >
               {info}
             </Typography>
           </View>
